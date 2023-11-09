245fab37785fa6143006bb2135bf17a29dfaa71a
irisco88 <iris1988noori@gmail.com>
2023-11-09 10:27:19 +0330

    add io_element_val

--- migrations/sqls/20230650092028_init.sql
+++ migrations/sqls/20230650092028_init.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE TABLE avlpointsData
     satellites  UInt8,
     speed       Int16,
     io_elements Map(String, Float64),
-    io_elementsVal Map(UInt16, Int64),,
+    io_elements_val Map(UInt16, Int64),,
 --    element_values Map(String, Float64),
     event_id    UInt16
 ) ENGINE = MergeTree()
